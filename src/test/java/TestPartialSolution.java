60e8684847ea878ebbac866c45e37ed394888a5d
dongmeilim <dongmeilim@gmail.com>
2020-08-13 21:25:32 +1200

    refactor partialsolution tests

--- src/test/java/TestPartialSolution.java
+++ src/test/java/TestPartialSolution.java
@@ -8,36 +8,62 @@ import static org.junit.Assert.fail;
 
 public class TestPartialSolution {
     private PartialSolution _ps;
+    private Graph _graph;
+    private ScheduledTask _st;
 
     @Before
-    public void setUp(){
+    public void setUp() throws Exception {
+        _graph = new Graph();
+        _graph.addNode("A", 3);
+        _graph.addNode("B", 2);
+        _graph.addNode("C", 4);
+        _graph.addEdge("A", "B", 1);
+        _graph.addEdge("A", "C", 5);
+
+        _st = new ScheduledTask(1, _graph.getNode("A"), 0);
+
+        _ps = new PartialSolution(null, _graph, _st);
     }
 
     @Test
-    public void testEquals() throws Exception {
-        Graph graph = new Graph();
-        graph.addNode("A", 3);
-        graph.addNode("B", 2);
-        graph.addNode("C", 4);
-        graph.addEdge("A", "B", 1);
-        graph.addEdge("A", "C", 5);
+    public void testEquals() {
+        PartialSolution ps2 = new PartialSolution(null, _graph, _st);
 
+        if (!_ps.equals(ps2)) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testEqualsContent() throws Exception {
+        Graph compare = new Graph();
+        compare.addNode("A", 3);
+        compare.addNode("B", 2);
+        compare.addNode("C", 4);
+        compare.addEdge("A", "B", 1);
+        compare.addEdge("A", "C", 5);
+        ScheduledTask st2 = new ScheduledTask(1, compare.getNode("A"), 0);
+        PartialSolution ps2 = new PartialSolution(null, compare, st2);
+
+        if (!_ps.equals(ps2)) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testNotEquals() throws Exception {
 
         Graph graph2 = new Graph();
-        graph2.addNode("A", 3);
-        graph2.addNode("B", 2);
-        graph2.addNode("C", 4);
+        graph2.addNode("A", 1);
+        graph2.addNode("B", 1);
+        graph2.addNode("C", 1);
         graph2.addEdge("A", "B", 1);
-        graph2.addEdge("A", "C", 5);
-
-        ScheduledTask st = new ScheduledTask(1, graph.getNode("A"), 0);
+        graph2.addEdge("A", "C", 1);
 
         ScheduledTask st2 = new ScheduledTask(1, graph2.getNode("A"), 0);
-
-        _ps = new PartialSolution(null, graph, st);
-
         PartialSolution ps2 = new PartialSolution(null, graph2, st2);
-        if (!_ps.equals(ps2)){
+
+        if (_ps.equals(ps2)) {
             fail();
         }
     }
