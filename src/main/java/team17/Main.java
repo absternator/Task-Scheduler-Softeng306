5a71313b33c38aff350564def8f8b395ec600d76
Annie Chau <annithinggoes@gmail.com>
2020-08-13 21:09:23 +1200

    Able to write edges and nodes out to dot file in alphabetical order. Included temp code for running the program

--- src/main/java/team17/Main.java
+++ src/main/java/team17/Main.java
@@ -7,6 +7,7 @@ import team17.DAG.Graph;
 import team17.DAG.Node;
 
 import java.io.*;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -14,10 +15,16 @@ public class Main {
     private static String _outputFileName;
     private static String _inputFileName = "";
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int nProcessors;
         boolean visualise;
         int nCores;
+
+        //Run from command line
+        //args = new String[]{"../../src/main/resources/graph.dot", "2"};
+
+        //Run in IDE
+        args = new String[]{"src/main/resources/graph.dot", "2"};
 
         Options options = new Options();
 
@@ -30,8 +37,8 @@ public class Main {
         options.addOption(outputOpt);
 
         //Check first two args, then options
-        if(args.length>=2) {
-            if(args[0].endsWith(".dot")) {
+        if (args.length >= 2) {
+            if (args[0].endsWith(".dot")) {
                 _inputFileName = args[0];
             }
             nProcessors = Integer.parseInt(args[1]);
@@ -43,11 +50,11 @@ public class Main {
             cmd = parser.parse(options, args);
             visualise = cmd.hasOption("v");
             // getOptionValue returns null if no argument
-            if(cmd.getOptionValue("p")!=null){
+            if (cmd.getOptionValue("p") != null) {
                 nCores = Integer.parseInt(cmd.getOptionValue("p"));
             }
             _outputFileName = cmd.getOptionValue("o");
-            if(_outputFileName!=null){
+            if (_outputFileName != null) {
             }
         } catch (ParseException e) {
             e.printStackTrace();
@@ -55,7 +62,13 @@ public class Main {
 
         try {
             // "src/main/resources/graph.dot"
-            readDotFile(_inputFileName);
+            Graph graph = readDotFile(_inputFileName);
+
+            AlgorithmAStar aStar = new AlgorithmAStar(graph);
+            List<ScheduledTask> schedule = aStar.getOptimalSchedule(); // Returns list of Schedule
+
+            writeOutput(schedule);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -63,9 +76,10 @@ public class Main {
 
     /**
      * This reads dot file
+     *
      * @throws IOException Throw IO exception if file does not exist.
      */
-    public static void readDotFile(String fileName) throws IOException {
+    public static Graph readDotFile(String fileName) throws IOException {
         Graph graph = new Graph();
         File file = new File(fileName);
         BufferedReader br = new BufferedReader(new FileReader(file));
@@ -77,10 +91,8 @@ public class Main {
         graph.addFinishNode();
         graph.setBottomLevel();
         graph.set_numOfProcessors(2);// Test : number of processors to run on
-        System.out.println(graph);
-        AlgorithmAStar aStar = new AlgorithmAStar(graph);
-        List<ScheduledTask> schedule = aStar.getOptimalSchedule(); // Returns list of Schedule
-        System.out.println(schedule);
+
+        return graph;
     }
 
     /**
@@ -88,15 +100,23 @@ public class Main {
      */
     private static void writeOutput(List<ScheduledTask> solution) throws IOException {
         String outputFileName = _outputFileName != null && !_outputFileName.isEmpty() ? _outputFileName : _inputFileName + "-output";
+        Collections.sort(solution);
 
-        File file = new File( outputFileName + ".dot");
+        File file = new File(outputFileName + ".dot");
         PrintWriter pw = new PrintWriter(new FileWriter(file));
 
         pw.println("digraph \"" + outputFileName + "\" {");
         for (ScheduledTask task : solution) {
             Node node = task.getNode();
-            pw.printf("%-10s[Weight=%d,Start=%d,Processor=%d]\n;",node.getId(),node.getWeight(),task.getStartTime(),task.getProcessorNum());
+            pw.printf("\t%-10s[Weight=%d,Start=%d,Processor=%d];\n", node.getId(), node.getWeight(), task.getStartTime(), task.getProcessorNum());
+            if (!node.getDependencies().isEmpty()) {
+                for (Node incoming : node.getDependencies()) {
+                    String entry = incoming.getId() + " -> " + node.getId();
+                    pw.printf("\t%-10s[Weight=%d];\n", entry, node.getIncomingEdges().get(incoming));
+                }
+            }
         }
+        pw.println("}");
         pw.close();
     }
 
