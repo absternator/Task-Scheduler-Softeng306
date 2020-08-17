7037106203761af625af05aee5b0ce146e2fe437
dongmeilim <dongmeilim@gmail.com>
2020-08-17 23:40:12 +1200

    account for processor numbers in using the bandaid

--- src/main/java/team17/Main.java
+++ src/main/java/team17/Main.java
@@ -21,7 +21,7 @@ public class Main {
         //args = new String[]{"../../src/main/resources/graph.dot", "2"};
 
         //Run in IDE
-        args = new String[]{"src/main/resources/graph.dot", "2"};
+        args = new String[]{"src/main/resources/graph2.dot", "2"};
 
         CLI cli = new CLI(args);
         _inputFileName = cli.getInput();
@@ -31,15 +31,18 @@ public class Main {
             // "src/main/resources/graph.dot"
             Graph graph = readDotFile(_inputFileName);
             List<ScheduledTask> schedule;
-            if (graph.getNodeList().size() < 10) {
+            if (graph.getNodeList().size() > 11
+                    || (graph.getNodeList().size() >10 && graph.getNumOfProcessors() > 3)
+                    || (graph.getNodeList().size() >9 && graph.getNumOfProcessors() > 6)) {
+                // for large graphs, use a band-aid solution (List scheduling)
+                ListScheduling bandaid = new ListScheduling(graph);
+                schedule = bandaid.getSchedule();
+
+            } else {
                 // for small graphs, use the A* algorithm
                 AlgorithmAStar aStar = new AlgorithmAStar(graph);
                 schedule = aStar.getOptimalSchedule(); // Returns list of Schedule
 
-            } else {
-                // for large graphs, use a band-aid solution (List scheduling)
-                ListScheduling bandaid = new ListScheduling(graph);
-                schedule = bandaid.getSchedule();
             }
 
             writeOutput(schedule);
