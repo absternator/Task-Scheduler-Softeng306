853c695456408f11f0f334d84f3bb743652e8e3c
Annie Chau <annithinggoes@gmail.com>
2020-08-22 13:53:51 +1200

    working application, removed non existent controller

--- src/main/java/team17/Main.java
+++ src/main/java/team17/Main.java
@@ -9,15 +9,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+<<<<<<< HEAD
 
 import team17.Algorithm.ScheduledTask;
 import team17.DAG.Graph;
+=======
+import team17.Algorithm.AlgorithmAStar;
+import team17.Algorithm.ListScheduling;
+import team17.Algorithm.ScheduledTask;
+import team17.DAG.Graph;
+import team17.GUI.visualiser;
+>>>>>>> working application, removed non existent controller
 import team17.IO.CLI;
 import team17.IO.FileReadWriter;
 
 import java.io.IOException;
 import java.util.List;
-import team17.GUI.visualiser;
 
 public class Main extends Application {
 
