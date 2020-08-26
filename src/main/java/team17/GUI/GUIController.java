6b65e30d55500539c08bed7fd4eedf2de27c15b5
xinyi <xguo679@aucklanduni.ac.nz>
2020-08-26 23:00:28 +1200

    add memory usage pane

--- src/main/java/team17/GUI/GUIController.java
+++ src/main/java/team17/GUI/GUIController.java
@@ -1,17 +1,66 @@
 package team17.GUI;
 
 
+import eu.hansolo.tilesfx.TileBuilder;
+import eu.hansolo.tilesfx.Tile;
+import eu.hansolo.tilesfx.TileBuilder;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+
 public class GUIController {
     @FXML
+    private Pane MemoryPane;
+
+
+    private Tile memoryUsageTile;
+    private double maxMemory;
+
 
     public void initialize(){
+        //show memory usage
+        maxMemory=Runtime.getRuntime().maxMemory()/1048576; // in bytes
+        setUpMemoryPane();
+        memoryUsageTile.setValue(0);
+    }
 
+    private void readValue(){
+
+    }
+
+    public void setUpInputFileName(){
+
+    }
+
+    public void setUpOutputFileName(){
+
+    }
+
+    public void setUpNumberOfProcessors(){
+
+    }
+
+    public void setUpStatus(){
+
+    }
+
+    public void setUpTime(){
+
+    }
+
+    private void setUpMemoryPane(){
+        this.memoryUsageTile = TileBuilder.create()
+                .skinType(Tile.SkinType.GAUGE)
+                .maxValue(maxMemory)
+                .unit("MB")
+                .startFromZero(true)
+                .animated(true)
+                .build();
+        MemoryPane.getChildren().addAll(memoryUsageTile);
     }
 
 
