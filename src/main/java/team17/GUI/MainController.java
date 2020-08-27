f49af44a029d9cd8e51076e3d9a15f968da657b3
xinyi <xguo679@aucklanduni.ac.nz>
2020-08-27 12:21:06 +1200

    show input file name

--- src/main/java/team17/GUI/MainController.java
+++ src/main/java/team17/GUI/MainController.java
@@ -1,10 +1,10 @@
 package team17.GUI;
 
 
-
 import eu.hansolo.tilesfx.TileBuilder;
 import eu.hansolo.tilesfx.Tile;
 import eu.hansolo.tilesfx.TileBuilder;
+import eu.hansolo.tilesfx.colors.Bright;
 import javafx.collections.FXCollections;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -15,6 +15,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.util.Duration;
 import team17.Algorithm.AlgorithmState;
 import team17.GUI.GanttChart.GanttChart;
@@ -25,8 +27,20 @@ public class MainController {
 
     @FXML
     private Pane MemoryPane;
+    @FXML
+    private TextFlow InputText;
+    @FXML
+    private TextFlow OutputText;
+    @FXML
+    private TextFlow ProcessorsNumberText;
+    @FXML
+    private Text StatusText;
+
     private Tile memoryUsageTile;
     private double maxMemory;
+    private int processorsNumber;
+    private String InputFile;
+    private String OutputFile;
 
     private CLI _config;
     private AlgorithmState _algorithmState;
@@ -41,11 +55,16 @@ public class MainController {
 
 
     public void init() {
+        setUpInputFileName();
+        setUpOutputFileName();
+        setUpNumberOfProcessors();
         //show memory usage
         maxMemory=Runtime.getRuntime().maxMemory()/1048576; // in bytes
         setUpMemoryPane();
         memoryUsageTile.setValue(0);
         readValue();
+        startTiming();
+
 
     }
     private void readValue(){
@@ -57,6 +76,8 @@ public class MainController {
         if(finished) {
                     finished = false;
                     tm.stop();
+                    setUpOutputFileName();
+                    UpdateStatus();
                 }
          */
         } ));
@@ -66,34 +87,58 @@ public class MainController {
     }
 
     public void setUpInputFileName(){
-
+        InputFile=_config.getInput();
+        InputFile = InputFile.substring(InputFile.lastIndexOf('/')+1);
+        InputFile = "  " + InputFile;
+        Text iText = new Text (InputFile);
+        iText.setStyle("-fx-font: 22 System;");
+        InputText.getChildren().add(iText);
     }
 
     public void setUpOutputFileName(){
-
+        OutputFile=_config.getOutput();
+        OutputFile="src/main/graphout.dot";
+        OutputFile = OutputFile.substring(OutputFile.lastIndexOf('/')+1);
+        OutputFile = "  "+OutputFile;
+        Text oText = new Text (OutputFile);
+        oText.setStyle("-fx-font: 22 System;");
+        OutputText.getChildren().add(oText);
     }
 
     public void setUpNumberOfProcessors(){
-
+        processorsNumber=_config.getProcessors();
+        String processorNumberString = String.valueOf(processorsNumber);
+        processorNumberString = "  "+ processorNumberString;
+        Text pText = new Text(processorNumberString);
+        pText.setStyle("-fx-font: 22 System;");
+        ProcessorsNumberText.getChildren().add(pText);
     }
 
     public void UpdateStatus(){
+        StatusText.setText("Done");
+    }
+
+    private void startTiming(){
 
     }
 
-    public void setUpTime(){
+    private void finishTiming(){
 
     }
+
 
     private void setUpMemoryPane(){
         this.memoryUsageTile = TileBuilder.create()
                 .skinType(Tile.SkinType.GAUGE)
                 .maxValue(maxMemory)
-                .threshold(maxMemory*0.9)
+                .threshold(maxMemory*0.85)
                 .thresholdVisible(false)
                 .unit("MB")
                 .startFromZero(true)
                 .animated(true)
+                //.backgroundColor(Color.WHITE)
+                .prefWidth(MemoryPane.getWidth())
+                .prefHeight(MemoryPane.getHeight())
                 .build();
         MemoryPane.getChildren().addAll(memoryUsageTile);
     }
