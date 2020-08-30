a5a281b7aa15685960e3ece412c779f6f3c6a582
JayZed <jzha700@aucklanduni.ac.nz>
2020-08-30 16:41:01 +1200

    Extend illegalArgumentException

--- src/main/java/team17/IO/IncorrectCLIInputException.java
+++ src/main/java/team17/IO/IncorrectCLIInputException.java
@@ -3,7 +3,7 @@ package team17.IO;
 /**
  * This class is the custom checked exception for reading CLI input
  */
-public class IncorrectCLIInputException extends Exception{
+public class IncorrectCLIInputException extends IllegalArgumentException{
     public IncorrectCLIInputException(String errMsg) {
         super(errMsg);
     }
